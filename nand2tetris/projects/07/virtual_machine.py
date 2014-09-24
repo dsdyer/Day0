ac5c3d3e78d25e345636bf3f5f2b1f3ecff03067
David Dyer <ddyer@leapfrogonline.com>
2014-09-24 18:29:05 -0500

    Open file or directory and get ready to parse

---
+++ nand2tetris/projects/07/virtual_machine.py
@@ -0,0 +1,30 @@
+import sys
+import re
+import glob
+
+class Parser(object):
+  """prepares inputs for translation into assembly code"""
+  def __init__(self, arg):
+    self.arg = arg
+    self.file = open(self.arg, "r")
+  def showLines(self):
+    for line in self.file:
+      print(line)
+
+
+
+if __name__ == "__main__":
+  target = sys.argv[1]
+
+  if re.search("\.vm$", str(target)):     # if the argument was one .vm file, parse it
+    file = sys.argv[1]
+    p = Parser(file)
+    p.showLines()
+  else:                                   # if the argument is not a .vm file, try to
+    files = glob.glob(target + '/*.vm')   # treat it as a directory containing .vm files
+    if len(files) >= 1:
+      for x in files:
+        p = Parser(x)
+        p.showLines()
+    else:                                 # if that fails tell them
+      raise Exception('Bad input. You fix.')
