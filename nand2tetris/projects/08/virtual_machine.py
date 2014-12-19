c03e3241a7dbeb283eb008b87789356444c062dd
David Dyer <ddyer@leapfrogonline.com>
2014-12-19 16:38:24 -0600

    Write function

--- nand2tetris/projects/08/virtual_machine.py
+++ nand2tetris/projects/08/virtual_machine.py
@@ -15,9 +15,9 @@ class Parser(object):
       if re.match("//", line) or not re.match("[^\r\n]", line):
         pass # current line is blank or whitespace
       else:
-        self.commands.append(line.replace("\r", "").replace("\n", "").split(' '))
+        self.commands.append(line.replace("\r", "").replace("\n", "").split(" "))
     self.commands = iter(self.commands)
-    self.current = ''
+    self.current = ""
   
   def showLines(self):
     for line in self.file:
@@ -29,34 +29,34 @@ class Parser(object):
     
   def commandType(self):
     types = {
-      'add'      : 'C_ARITHMETIC',
-      'sub'      : 'C_ARITHMETIC',
-      'neg'      : 'C_ARITHMETIC',
-      'eq'       : 'C_ARITHMETIC',
-      'gt'       : 'C_ARITHMETIC',
-      'lt'       : 'C_ARITHMETIC',
-      'and'      : 'C_ARITHMETIC',
-      'or'       : 'C_ARITHMETIC',
-      'not'      : 'C_ARITHMETIC',
-      'push'     : 'C_PUSH',
-      'pop'      : 'C_POP',
-      'label'    : 'C_LABEL',
-      'goto'     : 'C_GOTO',
-      'if-goto'  : 'C_IF',
-      'function' : 'C_FUNCTION',
-      'return'   : 'C_RETURN',
-      'call'     : 'C_CALL',
+      "add"      : "C_ARITHMETIC",
+      "sub"      : "C_ARITHMETIC",
+      "neg"      : "C_ARITHMETIC",
+      "eq"       : "C_ARITHMETIC",
+      "gt"       : "C_ARITHMETIC",
+      "lt"       : "C_ARITHMETIC",
+      "and"      : "C_ARITHMETIC",
+      "or"       : "C_ARITHMETIC",
+      "not"      : "C_ARITHMETIC",
+      "push"     : "C_PUSH",
+      "pop"      : "C_POP",
+      "label"    : "C_LABEL",
+      "goto"     : "C_GOTO",
+      "if-goto"  : "C_IF",
+      "function" : "C_FUNCTION",
+      "return"   : "C_RETURN",
+      "call"     : "C_CALL",
     }
     return types[self.current[0]]
         
   def arg1(self):
-    if self.commandType() == 'C_ARITHMETIC':
+    if self.commandType() == "C_ARITHMETIC":
       return self.current[0]
     else:
       return self.current[1]
 
   def arg2(self):
-    if self.commandType() == 'C_PUSH':
+    if self.commandType() == "C_PUSH":
       return self.current[2]
 
 class CodeWriter(object):
@@ -112,9 +112,9 @@ class CodeWriter(object):
       ]
     elif command[0] == "eq":
       labels = { 
-        "TRUE"  : self.uniqueLabel('TRUE'), 
-        "FALSE" : self.uniqueLabel('FALSE'), 
-        "END"   : self.uniqueLabel('END')
+        "TRUE"  : self.uniqueLabel("TRUE"), 
+        "FALSE" : self.uniqueLabel("FALSE"), 
+        "END"   : self.uniqueLabel("END")
       }
       assembly = [
         "@SP",
@@ -145,9 +145,9 @@ class CodeWriter(object):
       ]
     elif command[0] == "gt":
       labels = { 
-        "TRUE"  : self.uniqueLabel('TRUE'), 
-        "FALSE" : self.uniqueLabel('FALSE'), 
-        "END"   : self.uniqueLabel('END')
+        "TRUE"  : self.uniqueLabel("TRUE"), 
+        "FALSE" : self.uniqueLabel("FALSE"), 
+        "END"   : self.uniqueLabel("END")
       }
       assembly = [
         "@SP",
@@ -178,9 +178,9 @@ class CodeWriter(object):
       ]
     elif command[0] == "lt":
       labels = { 
-        "TRUE"  : self.uniqueLabel('TRUE'), 
-        "FALSE" : self.uniqueLabel('FALSE'), 
-        "END"   : self.uniqueLabel('END')
+        "TRUE"  : self.uniqueLabel("TRUE"), 
+        "FALSE" : self.uniqueLabel("FALSE"), 
+        "END"   : self.uniqueLabel("END")
       }
       assembly = [
         "@SP",
@@ -240,7 +240,7 @@ class CodeWriter(object):
         "M=M+1"
       ]
     else:
-      raise(Exception('Something isn\'t right!'))
+      raise(Exception("Something isn\"t right!"))
     return assembly
   
   def writePushPop(self, c):
@@ -304,17 +304,17 @@ class CodeWriter(object):
       # Whatever that means
     ]
   def writeLabel(self, c):
-    assembly = ['(' + self.file_name + '$' + self.working_parser.arg1() + ')']
+    assembly = ["(" + self.file_name + "$" + self.working_parser.arg1() + ")"]
     return assembly
   def writeGoto(self, c):
-    label = self.file_name + '$' + self.working_parser.arg1()
+    label = self.file_name + "$" + self.working_parser.arg1()
     assembly = [
       "@" + label,
       "0;JMP"
     ]
     return assembly
   def writeIf(self, c):
-    label = self.file_name + '$' + self.working_parser.arg1()
+    label = self.file_name + "$" + self.working_parser.arg1()
     assembly = [
       "@SP",
       "AM=M-1",
@@ -324,10 +324,10 @@ class CodeWriter(object):
     ]
     return assembly
   def writeCall(self, c):
-    return_address = self.uniqueLabel('return_address')
+    return_address = self.uniqueLabel("return_address")
     args = c[2]
     segments = [return_address, "LCL", "ARG", "THIS", "THAT"]
-    assembly = ['call: ']
+    assembly = []
     for a in segments:
       assembly.extend([
         "@" + a,
@@ -356,11 +356,13 @@ class CodeWriter(object):
     return assembly
 
   def writeReturn(self, c):
-    assembly = ['return']
+    assembly = ["return"]
     return assembly
   def writeFunction(self, c):
     self.setFileName(self.working_parser.arg1())
-    assembly = [self.file_name]
+    assembly = ["(" + self.file_name + ")"]
+    for n in range(int(c[2])):
+      assembly.extend(self.writePushPop(["push", "constant", "0"]))
     return assembly
     
 if __name__ == "__main__":
@@ -371,13 +373,13 @@ if __name__ == "__main__":
     p = Parser(target)
     parsers.append(p)
   else:                                   # if the argument is not a .vm file, try to
-    files = glob.glob(target + '/*.vm')   # treat it as a directory containing .vm files
+    files = glob.glob(target + "/*.vm")   # treat it as a directory containing .vm files
     if len(files) >= 1:
       for x in files:
         p = Parser(x)
         parsers.append(p)
     else:                                 # if that fails tell them
-      raise Exception('Input is not a VM file or directory. Getting kinda tired of your shit.')
+      raise Exception("Input is not a VM file or directory. Getting kinda tired of your shit.")
 
   x = CodeWriter()
 
@@ -387,21 +389,21 @@ if __name__ == "__main__":
       try: 
         c = x.working_parser.advance()
         t = x.working_parser.commandType()
-        if t == 'C_ARITHMETIC':
+        if t == "C_ARITHMETIC":
           x.output_file.write("\n".join(x.writeArithmetic(c)) + "\n")
-        elif t in ('C_PUSH', 'C_POP'):
+        elif t in ("C_PUSH", "C_POP"):
           x.output_file.write("\n".join(x.writePushPop(c)) + "\n")
-        if t == 'C_LABEL':
+        if t == "C_LABEL":
           x.output_file.write("\n".join(x.writeLabel(c)) + "\n")
-        if t == 'C_GOTO':
+        if t == "C_GOTO":
           x.output_file.write("\n".join(x.writeGoto(c)) + "\n")
-        if t == 'C_IF':
+        if t == "C_IF":
           x.output_file.write("\n".join(x.writeIf(c)) + "\n")
-        if t == 'C_CALL':
+        if t == "C_CALL":
           x.output_file.write("\n".join(x.writeCall(c)) + "\n")
-        if t == 'C_RETURN':
+        if t == "C_RETURN":
           x.output_file.write("\n".join(x.writeReturn(c)) + "\n")
-        if t == 'C_FUNCTION':
+        if t == "C_FUNCTION":
           x.output_file.write("\n".join(x.writeFunction(c)) + "\n")
       except(StopIteration):
         break
